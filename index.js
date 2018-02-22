cbe3fd3eda6a4f44788c1e330e478d0892d54695
Jason Mojica <mojicaj@gmail.com>
2018-02-22 10:20:24 -0500

    change index.js message

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello world!');
+  res.send("Hello Bears-27!<br>Let's rock and roll!");
 });
 
 app.listen(5000);
