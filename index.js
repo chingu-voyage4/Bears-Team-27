c233b1be779a75c23acf571fd118e7c7da07b04a
Tia <msksfo@gmail.com>
2018-02-25 06:45:51 -0800

    Just practicing

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Good morning Bears!');
+  res.send('Have a great weekend everyone!');
 });
 
 app.listen(5000);
