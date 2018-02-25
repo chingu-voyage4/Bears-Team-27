ba05b524f54bcc742d6b0391a38ddde66060052d
Tia <msksfo@gmail.com>
2018-02-25 06:58:28 -0800

    still practicing

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Good morning Bears!');
+  res.send("Yeah for Sunday FUNday");
 });
 
 app.listen(5000);
