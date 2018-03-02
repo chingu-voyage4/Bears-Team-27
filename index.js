e5684471266cd6cd5bb5fe206247543e4b3ae057
Jarold Wong <jaroldw@gmail.com>
2018-03-02 12:58:12 -0800

    setup dotenv

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 
 const app = express();
-const keys = require('./config/keys');
 
-mongoose.connect(keys.mongoURI);
+mongoose.connect(process.env.MONGO_URI);
 
 app.get('/', (req, res) => {
   res.send('Hello world!');
 });
 
-app.listen(5000);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
