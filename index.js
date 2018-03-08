cc01de9259b34ce56510b0d607c4eb5362fe2a09
Jarold Wong <jaroldw@gmail.com>
2018-03-08 12:51:42 -0800

    Passport configuration for Google OAuth2.0

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
-
-const app = express();
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 mongoose.connect(process.env.MONGO_URI);
+
+const app = express();
+app.use(passport.initialize());
+app.use(passport.session());
 
 if (process.env.NODE_ENV === 'production') {
   // serve up production assets inside client/build folder
@@ -16,6 +20,46 @@ if (process.env.NODE_ENV === 'production') {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
+
+// Passport Configuration
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: '/auth/google/callback',
+      proxy: true
+    },
+    (accessToken, refreshToken, profile, done) => {
+      // check if user exists in DB
+      // if exists, return existing user
+      // else save user in db and return user
+    }
+  )
+);
+
+// Auth Routes
+app.get('/auth/google', passport.authenticate('google'), {
+  scope: ['profile', 'email']
+});
+
+app.get(
+  '/auth/provider/callback',
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  (req, res) => {
+    res.redirect('/dashboard');
+  }
+);
+
+app.get('/logout', (req, res) => {
+  req.logout();
+  res.redirect('/');
+});
+
+// test route to see if user logged in
+app.get('/current_user', (req, res) => {
+  res.send(req.user);
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
