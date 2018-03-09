a315a9959bd9f3a148ed474fe047049ae9fd4d84
Jarold Wong <jaroldw@gmail.com>
2018-03-09 10:05:52 -0800

    merge differences

--- client/src/components/dashboard.js
+++ client/src/components/dashboard.js
@@ -1,24 +1,74 @@
 import React, { Component } from 'react';
-
-import ItemDetail from './ItemDetail';
+import Location from './Location';
 
 class Dashboard extends Component {
-  render () {
+  state = {
+    locations: []
+  };
+
+  addLocationHandler = () => {
+    const newLocation = {
+      name: 'New Location',
+      editMode: false
+    };
+    this.setState({
+      locations: [...this.state.locations, newLocation]
+    });
+  };
+
+  toggleEditLocationHandler = index => {
+    console.log(index);
+    const location = { ...this.state.locations[index] };
+    location.editMode = !location.editMode;
+
+    const locations = [...this.state.locations];
+    locations[index] = location;
+
+    this.setState({ locations: locations });
+  };
+
+  updateLocationHandler = (event, index) => {
+    const location = { ...this.state.locations[index] };
+    location.name = event.target.value;
+
+    const locations = [...this.state.locations];
+    locations[index] = location;
+
+    this.setState({
+      locations: locations
+    });
+  };
+
+  render() {
+    let addLocationText = 'Add a New Location to Begin';
+
+    if (this.state.locations.length > 0) {
+      addLocationText = 'Add More Locations';
+    }
+
     return (
-			<div className='items'>
-				<ItemDetail item="Sofa" room="Living Room" itemDescription="L-shaped sectional"
-					serialNumber="n/a" itemValue="$1000" purchaseDate="4 February, 2018" expDate="3 February, 2019"/>
-				<ItemDetail item="Dresser" room="Garage" itemDescription="Some Sporty Stuff"
-					serialNumber="84-3392-0974" itemValue="$2500" purchaseDate="29 October, 2000" expDate="28 October, 2003"/>
-				<ItemDetail item="Sporty Stuff" room="Guest Bedroom" itemDescription="It's a bed"
-					serialNumber="n/a" itemValue="$1300" purchaseDate="1 January, 2018" expDate="31 December, 2028"/>
-				<ItemDetail item="Nespresso Machine" room="Kitchen" itemDescription="Super fancy coffee machine"
-					serialNumber="84-3392-0974" itemValue="$599" purchaseDate="29 October, 2015" expDate="n/a"/>
-				<ItemDetail item="Persian Rug" room="Living Room" itemDescription="Souvenir from Persia"
-					serialNumber="n/a" itemValue="priceless" purchaseDate="10 June, 2011" expDate="n/a"/>
-			</div>
-  );
+      <div className="container">
+        <h2>Dashboard</h2>
+        <div className="row">
+          <div className="col">
+            <button onClick={this.addLocationHandler}>{addLocationText}</button>
+          </div>
+        </div>
+        <div className="d-flex flex-wrap">
+          {this.state.locations.length > 0
+            ? this.state.locations.map((location, index) => (
+                <Location
+                  name={location.name}
+                  editMode={location.editMode}
+                  toggleEditMode={() => this.toggleEditLocationHandler(index)}
+                  changed={event => this.updateLocationHandler(event, index)}
+                  index={index}
+                />
+              ))
+            : null}
+        </div>
+      </div>
+    );
   }
 }
-
 export default Dashboard;
--- client/src/components/header.js
+++ client/src/components/header.js
@@ -1,27 +1,49 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Header extends Component {
   render() {
+    let navItems = (
+      <li key="login" className="nav-item">
+        <a className="nav-link" onClick={this.props.login}>
+          Login
+        </a>
+      </li>
+    );
+
+    if (this.props.isAuthenticated) {
+      navItems = [
+        <li key="dashboard" className="nav-item">
+          <Link to="/dashboard" className="nav-link">
+            Dashboard
+          </Link>
+        </li>,
+        <li key="signout" className="nav-item">
+          <a className="nav-link" onClick={this.props.login}>
+            Signout
+          </a>
+        </li>
+      ];
+    }
+
     return (
       <header>
         <nav className="navbar navbar-expand-lg navbar-light bg-light justify-content-between">
-          <a className="navbar-brand" href="#">
+          <a className="navbar-brand" href="/">
             Inventoree
           </a>
           <ul className="navbar-nav">
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                Sign Up
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                Login
-              </a>
-            </li>
+            {/* 
+          <li className="nav-item">
+            <a className="nav-link" href="/auth/google">
+              Login with Google
+            </a>
+          </li>
+          */}
+            {navItems}
           </ul>
         </nav>
-    </header>
+      </header>
     );
   }
 }
