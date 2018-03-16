b23b389b8e18f59092adb4219627c37c6f04a33e
Jason Mojica <mojicaj@gmail.com>
2018-03-15 20:42:14 -0400

    create newUser function

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,2 +1,27 @@
 const User = require('../models/user');
 
+module.exports = {
+  newUser: newUser
+};
+
+
+// create a new user
+function newUser(req, res) {
+  const user = new User({
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    id: req.body.id,
+    locations: req.body.locations,
+    items: req.body.items
+  });
+
+  // save new user
+  user.save((err) => {
+    if (err) {
+      throw err;
+    }
+
+    res.redirect('/home');
+  });
+}
