7723387cfa0f51b854bafef88e18c2e5e2feb8c5
Jason Mojica <mojicaj@gmail.com>
2018-03-18 17:31:41 -0400

    create add item controller

--- controllers/inventory.controller.js
+++ controllers/inventory.controller.js
@@ -1,2 +1,35 @@
-const Item = require('../models/item'),
-      Location = require('../models/location');
+const Item = require('../models/item');
+
+
+// add item
+function addItem(req, res) {
+  // get item details
+  const item = new Item({
+    userId: req.body.userId,
+    locationId: req.body.location,
+    room: req.body.room,
+    name: req.body.name,
+    description: req.body.description,
+    serialNumber: req.body.serialNumber,
+    value: req.body.value,
+    warranty: req.body.warranty,
+    acquisition: req.body.acquisition,
+    images: req.body.images,
+  });
+
+  // save new item
+  item.save((err) => {
+    if (err) {
+      throw err;
+    }
+
+    res.redirect(`/home/${req.body.location}`);
+  });
+}
+
+
+// edit item
+
+// delete item
+
+module.exports = { addItem: addItem };
